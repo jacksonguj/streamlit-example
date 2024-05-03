0672dc73c363b6e292fd130173eb11069b761f85
jacksonguj <135077581+jacksonguj@users.noreply.github.com>
2024-05-03 22:39:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ import pandas as pd
 import streamlit as st
 
 st.title('SymptomSnap')
+
+import streamlit as st
+
+options = st.multiselect(
+    "What are your favorite colors",
+    ["Green", "Yellow", "Red", "Blue"],
+    ["Yellow", "Red"])
+
+st.write("You selected:", options)
