c5dd47d7594760e43df259b6c1c301edd0ccec50
jacksonguj <135077581+jacksonguj@users.noreply.github.com>
2024-05-04 20:15:45 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -24,7 +24,7 @@ if rad=="Symptom-Based Disease Guide":
     options = st.multiselect(
         "Choose Your Symptoms",
         ['itching', ' skin_rash', ' nodal_skin_eruptions', ' dischromic _patches', ' continuous_sneezing', ' shivering', ' chills', ' watering_from_eyes', ' stomach_pain', ' acidity', ' ulcers_on_tongue', ' vomiting', ' cough', ' yellowish_skin', ' nausea', ' loss_of_appetite', ' burning_micturition', ' spotting_ urination', ' abdominal_pain', ' passage_of_gases', ' indigestion', ' muscle_wasting', ' patches_in_throat', ' high_fever', ' extra_marital_contacts', ' fatigue', ' weight_loss', ' restlessness', ' lethargy', ' irregular_sugar_level', ' sunken_eyes', ' dehydration', ' diarrhoea', ' breathlessness', ' family_history', ' headache', ' chest_pain', ' dizziness', ' loss_of_balance', ' lack_of_concentration', ' blurred_and_distorted_vision', ' excessive_hunger', ' back_pain', ' weakness_in_limbs', ' neck_pain', ' weakness_of_one_body_side', ' altered_sensorium', ' sweating', ' joint_pain', ' dark_urine', ' yellowing_of_eyes', ' swelling_of_stomach', ' distention_of_abdomen', ' constipation', ' pain_during_bowel_movements', ' pain_in_anal_region', ' bloody_stool', ' irritation_in_anus', ' cramps', ' bruising', ' obesity', ' swollen_legs', ' weight_gain', ' cold_hands_and_feets', ' mood_swings', ' anxiety', ' knee_pain', ' hip_joint_pain', ' swelling_joints', ' muscle_weakness', ' stiff_neck', ' movement_stiffness', ' painful_walking', ' spinning_movements', ' pus_filled_pimples', ' blackheads', ' scurring', ' bladder_discomfort', ' foul_smell_of urine', ' continuous_feel_of_urine', ' skin_peeling', ' silver_like_dusting', ' small_dents_in_nails', ' blister', ' red_sore_around_nose', ' yellow_crust_ooze']
-        , [])
+        , [], key="symptom_multiselect")
 
     st.write("You selected:", options)
 
@@ -64,7 +64,7 @@ if rad=="Symptom-Based Disease Guide":
     disease_description = dict(zip(df['Disease'], df['Description']))
 
     # 선택된 질병에 따라 식단 표시
-    selected_diseases = st.multiselect("Select Diseases", df['Disease'].tolist())
+    selected_diseases = st.multiselect("Select Diseases", df['Disease'].tolist(), key="diagnosis_multiselect")
     for disease in selected_diseases:
         if disease in disease_description:
             st.write(f"Diagnosis Details ({disease}): {disease_description[disease]}")
@@ -80,7 +80,7 @@ if rad=="Symptom-Based Disease Guide":
     disease_diets = dict(zip(df['Disease'], df['Diet']))
 
     # 선택된 질병에 따라 식단 표시
-    selected_diseases = st.multiselect("Select Diseases", df['Disease'].tolist())
+    selected_diseases = st.multiselect("Select Diseases", df['Disease'].tolist(),key="diet_multiselect")
     for disease in selected_diseases:
         if disease in disease_diets:
             st.write(f"Dietary Recommendations ({disease}): {disease_diets[disease]}")
