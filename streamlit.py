afb601e8a682e710b9cfd12402c5a60fe89e890a
jacksonguj <135077581+jacksonguj@users.noreply.github.com>
2024-05-05 00:20:48 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -41,14 +41,23 @@ if rad == "Symptom-Based Disease Guide":
     # 모델 훈련
     rf_classifier.fit(X_train, y_train)
 
-    # 사용자가 선택한 증상에 대한 예측 확률
-    selected_symptoms_encoded = pd.get_dummies(pd.DataFrame([options], columns=X.columns))
-    predicted_probabilities = rf_classifier.predict_proba(selected_symptoms_encoded)
+    options = st.multiselect(
+        "Choose Your Symptoms",
+        ['itching', ' skin_rash', ' nodal_skin_eruptions', ' dischromic _patches', ' continuous_sneezing', ' shivering', ' chills', ' watering_from_eyes', ' stomach_pain', ' acidity', ' ulcers_on_tongue', ' vomiting', ' cough', ' yellowish_skin', ' nausea', ' loss_of_appetite', ' burning_micturition', ' spotting_ urination', ' abdominal_pain', ' passage_of_gases', ' indigestion', ' muscle_wasting', ' patches_in_throat', ' high_fever', ' extra_marital_contacts', ' fatigue', ' weight_loss', ' restlessness', ' lethargy', ' irregular_sugar_level', ' sunken_eyes', ' dehydration', ' diarrhoea', ' breathlessness', ' family_history', ' headache', ' chest_pain', ' dizziness', ' loss_of_balance', ' lack_of_concentration', ' blurred_and_distorted_vision', ' excessive_hunger', ' back_pain', ' weakness_in_limbs', ' neck_pain', ' weakness_of_one_body_side', ' altered_sensorium', ' sweating', ' joint_pain', ' dark_urine', ' yellowing_of_eyes', ' swelling_of_stomach', ' distention_of_abdomen', ' constipation', ' pain_during_bowel_movements', ' pain_in_anal_region', ' bloody_stool', ' irritation_in_anus', ' cramps', ' bruising', ' obesity', ' swollen_legs', ' weight_gain', ' cold_hands_and_feets', ' mood_swings', ' anxiety', ' knee_pain', ' hip_joint_pain', ' swelling_joints', ' muscle_weakness', ' stiff_neck', ' movement_stiffness', ' painful_walking', ' spinning_movements', ' pus_filled_pimples', ' blackheads', ' scurring', ' bladder_discomfort', ' foul_smell_of urine', ' continuous_feel_of_urine', ' skin_peeling', ' silver_like_dusting', ' small_dents_in_nails', ' blister', ' red_sore_around_nose', ' yellow_crust_ooze']
+        , [], key="symptom_multiselect")
 
+    st.write("You selected:", options)
+    
+    # 사용자가 선택한 증상에 대한 예측 확률
+    selected_symptoms = pd.DataFrame([options], columns=X.columns)  # 선택한 증상들을 행으로 가지는 데이터프레임 생성
+    selected_symptoms_encoded = pd.get_dummies(selected_symptoms)  # 더미 변수로 변환
+    
+    predicted_probabilities = rf_classifier.predict_proba(selected_symptoms_encoded)
+    
     # 상위 5개 질병 예측
     top_5_diseases = predicted_probabilities.argsort()[0][-5:][::-1]  # 상위 5개 질병의 인덱스
     top_5_probabilities = predicted_probabilities[0][top_5_diseases]  # 상위 5개 질병의 예측 확률
-
+    
     # 예측된 상위 5개 질병과 확률 출력
     st.subheader("Top 5 Predicted Diseases and Probabilities")
     for disease, probability in zip(top_5_diseases, top_5_probabilities):
