e15484dac66bb81af3adf47e8660df1288345225
jacksonguj <135077581+jacksonguj@users.noreply.github.com>
2024-05-05 01:15:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,57 +17,47 @@ if rad=="Home":
     st.header("Find Answer to Your Symptoms")
     st.text("Input your symptoms and discover possible conditions and treatments.")
 
-df = pd.read_csv('Disease_Symptom.csv')    
-# 특성과 타겟 데이터 준비
-X = df.drop('Disease', axis=1)
-y = pd.DataFrame(df['Disease'])
-    
-# 훈련 데이터와 테스트 데이터로 분할
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-# RandomForestClassifier 모델 생성
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-    
-# 모델 훈련
-rf_classifier.fit(X_train, y_train)
-    
 
-if rad == "Symptom-Based Disease Guide":
-    # 위의 코드와 같은 내용
-    
-    #### 병 예측
-    # CSV 파일 읽기
-    df = pd.read_csv('Disease_Symptom.csv')
-    
-    # 사용자가 선택한 증상들 입력 받기
+if rad=="Symptom-Based Disease Guide":
+    st.title('SymptomSnap')
+    st.subheader("Predicting Diseases from Symptoms")
     options = st.multiselect(
         "Choose Your Symptoms",
         ['itching', ' skin_rash', ' nodal_skin_eruptions', ' dischromic _patches', ' continuous_sneezing', ' shivering', ' chills', ' watering_from_eyes', ' stomach_pain', ' acidity', ' ulcers_on_tongue', ' vomiting', ' cough', ' yellowish_skin', ' nausea', ' loss_of_appetite', ' burning_micturition', ' spotting_ urination', ' abdominal_pain', ' passage_of_gases', ' indigestion', ' muscle_wasting', ' patches_in_throat', ' high_fever', ' extra_marital_contacts', ' fatigue', ' weight_loss', ' restlessness', ' lethargy', ' irregular_sugar_level', ' sunken_eyes', ' dehydration', ' diarrhoea', ' breathlessness', ' family_history', ' headache', ' chest_pain', ' dizziness', ' loss_of_balance', ' lack_of_concentration', ' blurred_and_distorted_vision', ' excessive_hunger', ' back_pain', ' weakness_in_limbs', ' neck_pain', ' weakness_of_one_body_side', ' altered_sensorium', ' sweating', ' joint_pain', ' dark_urine', ' yellowing_of_eyes', ' swelling_of_stomach', ' distention_of_abdomen', ' constipation', ' pain_during_bowel_movements', ' pain_in_anal_region', ' bloody_stool', ' irritation_in_anus', ' cramps', ' bruising', ' obesity', ' swollen_legs', ' weight_gain', ' cold_hands_and_feets', ' mood_swings', ' anxiety', ' knee_pain', ' hip_joint_pain', ' swelling_joints', ' muscle_weakness', ' stiff_neck', ' movement_stiffness', ' painful_walking', ' spinning_movements', ' pus_filled_pimples', ' blackheads', ' scurring', ' bladder_discomfort', ' foul_smell_of urine', ' continuous_feel_of_urine', ' skin_peeling', ' silver_like_dusting', ' small_dents_in_nails', ' blister', ' red_sore_around_nose', ' yellow_crust_ooze']
         , [], key="symptom_multiselect")
-    
+
     st.write("You selected:", options)
-    
-    # 사용자가 선택한 증상에 대한 예측 확률
-    selected_symptoms = pd.DataFrame(columns=X.columns)  # 빈 데이터프레임 생성
-    selected_symptoms.loc[0] = 0  # 첫 번째 행에 0으로 초기화
-    for symptom in options:
-        selected_symptoms[symptom] = 1  # 사용자가 선택한 증상들에 해당하는 열을 1로 설정
 
+    #### 병
+    # CSV 파일 읽기
+    df = pd.read_csv('Disease_Symptom.csv')
 
-    
-    predicted_probabilities = rf_classifier.predict_proba(selected_symptoms)
-    
-    # 상위 5개 질병 예측
-    top_5_diseases = predicted_probabilities.argsort()[0][-5:][::-1]  # 상위 5개 질병의 인덱스
-    top_5_probabilities = predicted_probabilities[0][top_5_diseases]  # 상위 5개 질병의 예측 확률
-    
-    # 예측된 상위 5개 질병과 확률 출력
-    st.subheader("Top 5 Predicted Diseases and Probabilities")
-    for disease, probability in zip(top_5_diseases, top_5_probabilities):
-        st.write(f"Disease: {rf_classifier.classes_[disease]}, Probability: {probability:.4f}")
+    # 사용자가 선택한 증상 필터링
+    filtered_df = df[df['Symptom_1'].isin(options) |
+                     df['Symptom_2'].isin(options) |
+                     df['Symptom_3'].isin(options) |
+                     df['Symptom_4'].isin(options)]
 
+    # 병별로 증상의 유사도 계산
+    disease_counts = filtered_df['Disease'].value_counts().reset_index()
+    disease_counts.columns = ['Disease', 'Count']
 
-    
+    # 상위 N개의 질병 선택 (여기서는 10개로 설정)
+    top_n = 5
+    top_diseases = disease_counts.head(top_n)
+
+    # 막대 그래프 생성
+    bar_chart = alt.Chart(top_diseases).mark_bar().encode(
+        x='Count',
+        y=alt.Y('Disease', sort='-x'),
+        color='Disease'
+    ).properties(
+        title='Top 5 most likely diseases'
+    )
+
+    # 시각화 결과 출력
+    st.write(bar_chart)
+
     #### 병 설명
     st.subheader("Explaining Your Diagnosis")
     df = pd.read_csv('Disease_Description.csv')
